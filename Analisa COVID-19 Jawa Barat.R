73e2aa66760b35249e42111da2e1bda4af13843c
Adi Suripto <73458569+adi1497@users.noreply.github.com>
2022-06-08 11:59:06 +0700

    Update Analisa COVID-19 Jawa Barat.R

--- Analisa COVID-19 Jawa Barat.R
+++ Analisa COVID-19 Jawa Barat.R
@@ -2,10 +2,12 @@
 library(ggplot2)
 library(httr)
 library(dplyr)
+library(tidyr)
 library(hrbrthemes)
 library(lubridate)
 
 set_config(config(ssl_verifypeer = 0L))
+# Akses API
 resp_jabar <- GET("https://storage.googleapis.com/dqlab-dataset/prov_detail_JAWA_BARAT.json")
 cov_jabar_raw <- content(resp_jabar, as = "parsed", simplifyVector = TRUE)
 
@@ -82,3 +84,89 @@ ggplot(new_cov_jabar, aes(tanggal, meninggal)) +
     ticks = TRUE
   ) +
   theme(plot.title.position = "plot")
+cov_jabar_pekanan <-
+  cov_jabar_pekanan %>% 
+  mutate(
+    jumlah_pekanlalu = dplyr::lag(jumlah, 1),
+    jumlah_pekanlalu = ifelse(is.na(jumlah_pekanlalu), 0, jumlah_pekanlalu),
+    lebih_baik = jumlah < jumlah_pekanlalu
+  )
+glimpse(cov_jabar_pekanan)
+
+ggplot(cov_jabar_pekanan[cov_jabar_pekanan$tahun==2020,], aes(pekan_ke, jumlah, fill = lebih_baik))+
+  geom_col(show.legend = FALSE) +
+  scale_x_continuous(breaks = 9:29,expand = c(0,0))+
+  scale_fill_manual(values = c("TRUE" = "seagreen3", "FALSE" = "salmon"))+
+  labs(
+    x = NULL,
+    y = "Jumlah Kasus",
+    title = "Kasus Pekanan Positif COVID-19 di Jawa Barat",
+    subtitle = "Kolom hijau menunjukan penambahan kasus baru lebih sedikit dibandingkan satu pekan sebelumnya",
+    caption = "Sumber data: covid.19.go.id"
+  ) +
+  theme_ipsum(
+    base_size = 13,
+    plot_title_size = 21, 
+    grid = "Y",
+    ticks = TRUE
+  ) +
+  theme(plot.title.position = "plot")
+
+cov_jabar_akumulasi <- 
+  new_cov_jabar %>% 
+  transmute(
+    tanggal,
+    akumulasi_aktif = cumsum(kasus_baru) - cumsum(sembuh) - cumsum(meninggal),
+    akumulasi_sembuh = cumsum(sembuh),
+    akumulasi_meninggal = cumsum(meninggal)
+  )
+
+tail(cov_jabar_akumulasi)
+
+ggplot(data = cov_jabar_akumulasi, aes(x = tanggal, y = akumulasi_aktif)) + geom_line()
+
+dim(cov_jabar_akumulasi)
+
+cov_jabar_akumulasi_pivot <- 
+  cov_jabar_akumulasi %>% 
+  gather(
+    key = "kategori",
+    value = "jumlah",
+    -tanggal
+  ) %>% 
+  mutate(
+    kategori = sub(pattern = "akumulasi_", replacement = "", kategori)
+  )
+
+dim(cov_jabar_akumulasi_pivot)
+
+glimpse(cov_jabar_akumulasi_pivot)
+
+ggplot(cov_jabar_akumulasi_pivot, aes(tanggal, jumlah, colour = (kategori)))+
+  geom_line(size = 0.9) +
+  scale_y_continuous(sec.axis = dup_axis(name = NULL))+
+  scale_colour_manual(
+    values = c(
+      "aktif" = "salmon",
+      "meninggal" = "darkslategray4",
+      "sembuh" = "olivedrab2"
+    ),
+    labels = c("Aktif", "Meninggal", "Sembuh")
+  ) +
+  labs(
+    x = NULL,
+    y = "Jumlah kasus akumulasi",
+    colour = NULL,
+    title = "Dinamika Kasus COVID-19 di Jawa Barat",
+    caption = "Sumber data: covid.19.go.id"
+  ) +
+  theme_ipsum(
+    base_size = 13,
+    plot_title_size = 21,
+    grid = "Y",
+    ticks = TRUE
+  ) +
+  theme(
+    plot.title = element_text(hjust = 0.5),
+    legend.position = "top"
+  )
